eab64a1d2394e6352a453ac0f2d919393b3ef829
SpDly <vshivaprasad07@gmail.com>
2022-08-07 15:47:01 +0530

    Update mainActivity.script.js

--- calendarKomponent/main/mainActivity.script.js
+++ calendarKomponent/main/mainActivity.script.js
@@ -2,10 +2,11 @@ console.log('JavaScript loaded successfully')
 const d = new Date();
 let date = d.getDate();
 
-const Month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-let month = Month[d.getMonth];
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let month = months[d.getMonth()];
 let year = d.getFullYear();
 
 console.log('Link = '+'https://raw.githubusercontent.com/SpDly14/calendarKomponent/main/calendarKomponent/src/cache/'+year+'/'+month+'/'+date+'.png')
 console.log('Redirecting now')
+setTimeout = 3000
 window.location.href = 'https://raw.githubusercontent.com/SpDly14/calendarKomponent/main/calendarKomponent/src/cache/'+year+'/'+month+'/'+date+'.png';
